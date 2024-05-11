a45c6f726fb52c1b34b9c0b13926eae9262b6218
Mihail Vorobyev <viklover@yandex.ru>
2024-05-11 23:03:19 +0500

    fix

--- rpi/main.py
+++ rpi/main.py
@@ -12,6 +12,10 @@ client = Backend(backend_url)
 
 while True:
     for sensor in sensors:
+        print(f"Считываю данные с датчика: {sensor}", end='')
         value = read_temp(sensor)
+        print(".. ok")
+        print("Отправление показаний в BackEnd", end='')
         client.save_value(sensor, value)
+        print(".. ok")
     time.sleep(delay)
--- rpi/temp.py
+++ rpi/temp.py
@@ -2,7 +2,7 @@ devices_dir = "/sys/bus/w1/devices"
 
 def read_temp(device_id):
     file = open(f'{devices_dir}/{device_id}/temperature')
-    value = int(file.readline())
+    value = int(file.readline().strip())
     value /= 1000
     file.close()
     return value
